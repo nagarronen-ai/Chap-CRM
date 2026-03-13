ca6e5b6a1247c99fd7fb0150892634bad32473a2
Dan Sitbon <dan.s@planfor.io>
2026-03-13 13:34:26 +0200

    Use env vars for Supabase credentials

--- server/db.js
+++ server/db.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(
-  'https://digtpiftmkwphubazdrq.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpZ3RwaWZ0bWt3cGh1YmF6ZHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4MDU5MDIsImV4cCI6MjA4NzM4MTkwMn0.vS1aeLuuk18qBcJq0hqk2ShcxWCYDhDw0vYyawYE4kM'
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 
 module.exports = supabase;
